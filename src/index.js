3fc5bfa1883327811b5078b520b5e361b5bcd33c
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-01 11:28:34 +0100

    Add js object as an inline styling to h1 element in jsx file.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+
+
+ReactDOM.render(<h1 style= {{color: "red"}}>Hello World!</h1>, document.getElementById("root"));
